0e11eae8fd1dadc5b24b8c933b2226a6eb4e9f29
yohobigcat <3109108399@qq.com>
2016-06-09 00:05:36 +0800

    modify test4

--- demo/test4/js/test4.js
+++ demo/test4/js/test4.js
@@ -2,15 +2,20 @@
  * Created by huahua on 2016/6/5.
  */
 $(function () {
-  $(".leftnavheader").bind("click", function () {
-    if ($("#hidval").val() == 0) {
-      $("dl").show("slow",function(){
-        $("#hidval").val(1);
-      })
-    } else {
-      $("dl").hide("slow",function(){
-        $("#hidval").val(0);
-      })
-    }
-  })
+  $(".leftnavheader").mouseover(function () {
+    $(".leftnavlist").show()
+    $(".leftnavheader span").removeClass().addClass("glyphicon glyphicon-menu-up")
+  });
+  $(".leftnavheader").mouseout(function () {
+    $(".leftnavlist").hide()
+    $(".leftnavheader span").removeClass().addClass("glyphicon glyphicon-menu-down")
+  });
+  $(".leftnavlist").mouseover(function () {
+    $(".leftnavlist").show()
+    $(".leftnavheader span").removeClass().addClass("glyphicon glyphicon-menu-up")
+  });
+  $(".leftnavlist").mouseout(function () {
+    $(".leftnavlist").hide()
+    $(".leftnavheader span").removeClass().addClass("glyphicon glyphicon-menu-down")
+  });
 });
